af72b846e7f82e5a50babbb82b88bd9290ad1663
KSreeHarsha <kharsha@eng.ucsd.edu>
2015-06-01 15:37:55 -0700

    .

--- module/zfs/metaslab.c
+++ module/zfs/metaslab.c
@@ -1757,9 +1757,9 @@ top:
 			goto next;
 		}
 
-		if((flags & ZIO_FLAG_TIER1) && vd->vdev_id==0)
-			goto next;
-		else if (vd->vdev_id!=0)
+		//if((flags & ZIO_FLAG_TIER1) && vd->vdev_id==0)
+			//goto next;
+		if (vd->vdev_id!=0)
 			goto next;
 
 
