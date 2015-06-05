ff95378f21c88fd1a34506ed60cb815ab426f6b5
KSreeHarsha <kharsha@eng.ucsd.edu>
2015-06-05 01:32:18 -0700

    .

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -1639,7 +1639,7 @@ zfs_ioc_pool_movet1t2(zfs_cmd_t *zc)
 	printk("Error while retriving spa\r\n");
 	#endif
 	 }
-	dump_zpool(spa,filename,filesize);
+	dump_zpool(spa,filename,filenum);
  	 #ifdef _KERNEL
 	printk("Filename: %s Filesize: %d:",filename,filenum);
 	printk("Move data from tier 1 to tier 2\r\n");
